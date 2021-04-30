68fe4c9fdfbb991578cda8a2ada6f794cef6d3a1
hgsgithub <62034695+hgsgithub@users.noreply.github.com>
2021-04-30 23:50:44 +0200

    30_04

--- AnimJq_21/AnimJquery.js
+++ AnimJq_21/AnimJquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("#animar").click(function(e){
 		e.preventDefault()
-		$("h1").animate({
+		$("h1").animate({   //parámetros,velocidad,easing, funcion callback
 			'border-bottom-width': "20",
 			'font-size': '25pt'
 		});
